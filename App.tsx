049f8ebeabb9193e55b43da22b8eb867ad4ce578
Siffster85 <ylsaxon@googlemail.com>
2024-03-22 12:10:14 +0000

    first commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This is the Start of OneTwoTrain</Text>
       <StatusBar style="auto" />
     </View>
   );

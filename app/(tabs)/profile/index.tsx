0ace4105612a1ba96d745a4aa51b704796c6bf78
Siffster85 <ylsaxon@googlemail.com>
2024-03-25 14:37:56 +0000

    Created button component with a circular theme

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -3,9 +3,10 @@ import { Stack } from "expo-router";
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
 import Button from '../../../components/Button';
+import { Alert } from "react-native";
 
 const profile = () => {
-  const [selectedImage, setSelectedImage] = useState<any>(null);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
   async function pickImageAsync() {
     try {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -15,12 +16,11 @@ const profile = () => {
   
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
-      console.log(result);
     } else {
       alert('You did not select any image.');
     }
-  } catch (error){
-    console.error(error)
+  } catch (error: any){
+    Alert.alert(error)
   }
   };
   return (
@@ -31,7 +31,6 @@ const profile = () => {
         headerTitleAlign: 'center'
         }}/>
         <Button theme="circular" label="Add Profile Photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" />
       <Text>Profile page</Text>
     </View>
   )

6ea1a8964cbbf2c16b81ed87188f0089d1b62acd
Calum <calum.borders@gmail.com>
2024-03-26 11:02:35 +0000

    Updated original card for exercise

--- app/(tabs)/plan/exercises/[id].tsx
+++ app/(tabs)/plan/exercises/[id].tsx
@@ -1,17 +1,51 @@
-import { Stack, router, useLocalSearchParams } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function currentExercise() {
   const { id, title } = useLocalSearchParams();
 
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       <Stack.Screen
         options={{
           headerTitle: `${title}`,
+          headerBackTitleVisible: false,
         }}
       />
-      <Text>Excerise page - {id}</Text>
-    </View>
+      <View style={{ flex: 3, backgroundColor: 'red' }}>
+        <View style={styles.card}>
+          <Text style={styles.title}>#SET1</Text>
+            <View>
+              <Text style={styles.infoText}>Weight: 50kg </Text>
+              <Text style={styles.infoText}>Reps: 50kg </Text>
+            </View>
+        </View>
+      </View>
+      <View style={{ flex: 1, backgroundColor: 'yellow' }}>
+
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  card: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: "white",
+    padding: 20,
+    justifyContent: 'space-between',
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 25,
+  },
+  infoText: {
+    fontSize: 15,
+  }
+});
